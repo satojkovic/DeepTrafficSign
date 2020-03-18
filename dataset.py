951b308d919dd619d2ac71ec3380fb9728e569de
satojkovic <satojkovic@gmail.com>
2020-03-19 08:43:41 +0900

    Add get_gt_csv

--- dataset.py
+++ dataset.py
@@ -1,7 +1,22 @@
 import pathlib
+import os
+import re
 
 class GTSRB:
     def __init__(self, data_root):
         self.data_root = pathlib.Path(data_root)
         self.train_dir = self.data_root.joinpath('Final_training')
         self.test_dir = self.data_root.joinpath('Final_test')
+        self._train_gt_csvs = self._get_gt_csvs(self.train_dir)
+        self._test_gt_csvs = self._get_gt_csvs(self.test_dir)
+
+    def _get_gt_csvs(self, root_dir):
+        gt_csvs = [
+            str(self.data_root.joinpath(f))
+            for root, dirs, files in os.walk(root_dir) for f in files
+            if re.search(r'.csv', f)
+        ]
+        return gt_csvs
+
+if __name__ == "__main__":
+    gtsrb = GTSRB('GTSRB')
