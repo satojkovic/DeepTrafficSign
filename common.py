efeac4e15f02fc6e96e9ebc1ca8be81937883f1e
satojkovic <satojkovic@gmail.com>
2022-09-17 21:20:57 +0900

    Use yacs

--- common.py
+++ common.py
@@ -24,21 +24,28 @@
 # -*- coding: utf-8 -*-
 import os
 import re
+from yacs.config import CfgNode
 
-GTSRB_ROOT_DIR = 'GTSRB'
+_C = CfgNode()
 
-TRAIN_ROOT_DIR = os.path.join(GTSRB_ROOT_DIR, 'Final_Training')
-TRAIN_PKL_FILENAME = 'traffic_sign_train_dataset.pickle'
-TRAIN_SIZE = len([
+_C.GTSRB_ROOT_DIR = 'GTSRB'
+
+_C.TRAIN_ROOT_DIR = os.path.join(_C.GTSRB_ROOT_DIR, 'Final_Training')
+_C.TRAIN_PKL_FILENAME = 'traffic_sign_train_dataset.pickle'
+_C.TRAIN_SIZE = len([
     f
-    for root, dirs, files in os.walk(os.path.join(TRAIN_ROOT_DIR, 'Images'))
+    for root, dirs, files in os.walk(os.path.join(_C.TRAIN_ROOT_DIR, 'Images'))
     for f in files if re.search(r'.ppm', f)
 ])
 
-TEST_ROOT_DIR = os.path.join(GTSRB_ROOT_DIR, 'Final_Test')
-TEST_PKL_FILENAME = 'traffic_sign_test_dataset.pickle'
-TEST_SIZE = len([
+_C.TEST_ROOT_DIR = os.path.join(_C.GTSRB_ROOT_DIR, 'Final_Test')
+_C.TEST_PKL_FILENAME = 'traffic_sign_test_dataset.pickle'
+_C.TEST_SIZE = len([
     f
-    for root, dirs, files in os.walk(os.path.join(TEST_ROOT_DIR, 'Images'))
+    for root, dirs, files in os.walk(os.path.join(_C.TEST_ROOT_DIR, 'Images'))
     for f in files if re.search(r'.ppm', f)
 ])
+
+
+def get_default_cfg():
+    return _C.clone()
